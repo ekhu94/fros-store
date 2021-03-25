8f429f87cf94f1c368889065a0e41c315853bf2b
Erik Huang <ekhu94@gmail.com>
2021-03-25 11:49:11 -0700

    first commit, scaffold index and App components, install dependencies

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className="ui header">Colin's eyebrows smell.</div>
+    );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import App from './components/App';
+
+// TODO implement Provider component from redux....maybe?
+
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+    document.querySelector('#root')
+);
