9c65cd06f972eff5f620ef27e6ba7cbb07290cce
Erik Huang <ekhu94@gmail.com>
2021-03-25 15:13:58 -0700

    finish relationships on serializers for cart and item

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const App = () => {
     return (
-        <div className="ui header">Colin's eyebrows smell.</div>
+        <div className="ui container">
+            Colin's eyebrows smell.
+        </div>
     );
 };
 
