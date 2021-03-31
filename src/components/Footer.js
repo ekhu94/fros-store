7d1596c4523bdb5604f014c5e8defb0a30369195
oopanpan <oopanpan@gmail.com>
2021-03-31 15:02:40 -0700

    footer adjusted

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,11 @@ import React from 'react'
 export default function Footer() {
 
     return (
-        <footer className="footer mt-auto py-4 bg-dark text-center">
+        <footer className="footer mt-auto py-4 bg-dark text-center" style={{opacity:0.9}}>
             <div className="container">
                 <span className="text-muted">
-                    Brought to you by <a href='https://github.com/ekhu94' target='_blank' style={{color:'white'}}>Erik Huang</a> and <a href='https://github.com/oopanpan' target='_blank' style={{color:'white'}}>Kai Peng Li</a> 
+                    Brought to you by <br></br>
+                    <a href='https://github.com/ekhu94' target='_blank' style={{color:'white'}}>Erik Huang</a> and <a href='https://github.com/oopanpan' target='_blank' style={{color:'white'}}>Kai Peng Li</a> 
                 </span>
             </div>
         </footer>
