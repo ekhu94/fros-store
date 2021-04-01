2f1ed9fb728ea3cd4576e2efe541c16caa9b0f65
Erik Huang <ekhu94@gmail.com>
2021-04-01 13:09:55 -0700

    ok ok the real last maxHeight adjustment

--- src/components/ClothCard.js
+++ src/components/ClothCard.js
@@ -53,14 +53,14 @@ export default function ClothCard({clothId}) {
                     <div className="row justify-content-center my-5">
                         <Image 
                             size='tiny'
-                            style={{maxHeight: '150px'}}
+                            style={{maxHeight: '140px'}}
                             src={cloth.front_URL}
                             alt={cloth.name}
                             onClick={()=>setFront(true)}
                         />
                         <Image
                             size='tiny'
-                            style={{maxHeight: '150px'}}
+                            style={{maxHeight: '140px'}}
                             src={cloth.back_URL}
                             alt={cloth.name}
                             onClick={()=>setFront(false)}
